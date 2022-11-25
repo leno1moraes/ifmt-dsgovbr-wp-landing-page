f1ac16e70ff72136c8681be182d65d67ae0e2466
3012056 <lenomoraes12@gmail.com>
2022-11-25 13:36:20 -0400

    Sergundo commit

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Carousel Template · Bootstrap v5.2</title>
+    <title> <?php bloginfo( 'name' ); ?> </title>
     
     <?php wp_head(); ?>
 
